f54e8cf3973d461453fa1fc6d42b88cc8c311fa0
vishalvsutar <vishalvsutar@gmail.com>
2022-09-12 00:32:11 -0400

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Vishal Sutar on 2022-09-12.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
